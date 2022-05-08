9fcb599e02842810b5adb7264ec2acfd38f2c4bf
neelxie <kidricederek@gmail.com>
2022-05-08 17:49:11 +0300

    creating and uploading metadat

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -8,7 +8,12 @@ contract NFT is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private currentTokenId;
 
-    constructor() ERC721("NFTT", "NFT") {}
+    // @dev Base token URI used as a prefix by tokenURI()
+    string public baseTokenURI;
+
+    constructor() ERC721("NFTT", "NFT") {
+        baseTokenURI = "";
+    }
 
     function mintTo(address recipient)
         public
@@ -17,5 +22,15 @@ contract NFT is ERC721 {
             uint256 newItemId = currentTokenId.current();
             _safeMint(recipient, newItemId);
             return newItemId;
+        }
+
+        // @dev Returns a URI for the given token ID
+        function _baseURI() internal view virtual override returns (string memory){
+            return baseTokenURI;
+        }
+
+        // @dev Sets the base token URI prefix
+        function setBaseTokenURI(string memory _baseTokenURI) public {
+            baseTokenURI = _baseTokenURI;
         }
       }
